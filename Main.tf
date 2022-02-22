f0ebfa866c6e9ea8680dbec3994810a7ce78ec1d
christoffer ulvestav <christoffer.ulvestav@outlook.com>
2022-02-22 12:52:25 +0100

    removed resource

--- Main.tf
+++ Main.tf
@@ -30,17 +30,5 @@ resource "azurerm_subnet" "subnet" {
 }
 
 
-resource "azurerm_subnet" "subnet" {
-
-  for_each = var.subnet
-
-  name                 = each.value["name"]
-  resource_group_name  = each.value["resource_group_name"]
-  virtual_network_name = each.value["virtual_network_name"]
-  address_prefixes     = each.value["address_prefixes"]
-  depends_on           = [azurerm_resource_group.this, azurerm_virtual_network.this]
-}
-
-
 
 
